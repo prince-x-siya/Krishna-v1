5092b1334de69d40c637c8f30c957078d4e2cdc6
prince-x-siya <108525561+prince-x-siya@users.noreply.github.com>
2025-04-10 20:55:46 +0530

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -1,14 +1,119 @@
-module.exports.config = {
-	name: "offbot",
-	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "turn the bot off",
-	commandCategory: "system",
-	cooldowns: 0
-        };
-module.exports.run = ({event, api}) =>{
-    const permission = ["61574844964675", "100017130165877"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For krishna", event.threadID, event.messageID);
-  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
-}
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261575097851906%22%3A%2274jXXEqy%22%2C%22100083933252759%22%3A%22BRKuHD5O%22%2C%22100036703381626%22%3A%22Vdu9RmH9%22%2C%2261574844964675%22%3A%22t77URkXU%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.876Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "datr",
+        "value": "ZK3sZ_9oxI7vuHTU2NtwCDbo",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "sb",
+        "value": "ZK3sZyHtlAM_NINzfCcPv5XN",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B712x360x2",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "locale",
+        "value": "en_GB",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61574844964675",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "fr",
+        "value": "01d0MCvSsTx7wpEUX.AWf7UHLK2TVQfDQkFcdg4fDPxmmcvfyyAa4G_qsPdvFY5MhVA_I.Bn7K1k..AAA.0.0.Bn9-JT.AWdjgAIPqA3aqwfiGmKCAUeVcOc",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.877Z",
+        "lastAccessed": "2025-04-10T15:25:14.877Z"
+    },
+    {
+        "key": "xs",
+        "value": "24%3AsRbsr3q1ma_99g%3A2%3A1744298580%3A-1%3A6096",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.878Z",
+        "lastAccessed": "2025-04-10T15:25:14.878Z"
+    },
+    {
+        "key": "wd",
+        "value": "360x712",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.878Z",
+        "lastAccessed": "2025-04-10T15:25:14.878Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2785%3Btimestamp%3A1744298592",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.878Z",
+        "lastAccessed": "2025-04-10T15:25:14.878Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "101033165%3BT%3A29071645",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-04-10T15:25:14.878Z",
+        "lastAccessed": "2025-04-10T15:25:14.878Z"
+    }
+]
